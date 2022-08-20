68c1a47b2382923c3ff88fef0ececac07ba664d9
SenerDemiral <sener.demiral@gmail.com>
2022-08-21 01:27:51 +0300

    #16

--- TntMud/Constants.cs
+++ TntMud/Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string PublishVersion = "0.6";
     public const string BrowserUsrIdKey = "uid";
-    public const string Height = "calc(100vh - 120px)";
+    public const string Height = "calc(80vh - 120px)";
 }
