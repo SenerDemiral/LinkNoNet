1fbd1360255aa160faae3534deb7ae40010be2d9
SenerDemiral <sener.demiral@gmail.com>
2022-08-25 22:45:39 +0300

    #19

--- TntMud/Constants.cs
+++ TntMud/Constants.cs
@@ -6,7 +6,7 @@ public static class Constants
     public const string BrowserUsrIdKey = "uid";
     public const string Height = "calc(80vh - 120px)";
 
-    public enum dStus { Açık = 0, Kabul = 1, Red = -2 };
+    public enum dStus { Açık = 0, Kabul = 1, Red = -1, Yasaklı = -9 };
     public enum kStus { Katılmadın = 0, Katılırım = 1, Vazgeçtim = -1, Seçildin = 2, Seçilmedin = -2 };
     public enum tStus { İptal = -1, KatılımBaşlamadı = 0, KatılımSürüyor = 1, KatılımBitti = 2, Kapandı = 3, Arşiv = 9 };
 
