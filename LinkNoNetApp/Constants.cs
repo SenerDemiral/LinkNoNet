8e8cff31f77f030a64760a00a70330871948c508
SenerDemiral <sener.demiral@gmail.com>
2022-09-13 17:07:47 +0300

    #26

--- LinkNoNetApp/Constants.cs
+++ LinkNoNetApp/Constants.cs
@@ -4,10 +4,7 @@ public static class Constants
 {
     public const string PublishVersion = "0.6";
     public const string BrowserUsrIdKey = "uid";
-    public const string Height = "calc(100vh - 120px)";
-
-    public enum dStus { Açık = 0, Kabul = 1, Red = -1, Yasaklı = -9 };
-    public enum kStus { Katılmadın = 0, Katılırım = 1, Vazgeçtim = -1, Seçildin = 2, Seçilmedin = -2 };
-    public enum tStus { İptal = -1, KatılımBaşlamadı = 0, KatılımSürüyor = 1, KatılımBitti = 2, Kapandı = 3, Arşiv = 9 };
+    public const string Height = "calc(100vh - 140px)";
+    public enum ttStus { LoginBekliyor = -2, OnayBekliyor = -1, Aktif = 0, InAktif = 1, Durduruldu = 2 };
 
 }
